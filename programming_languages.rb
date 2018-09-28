cd97c052056453fe3bf4e374f3de60146b0ba3a4
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-28 17:40:52 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
 def reformat_languages(languages)
-  # your code here
+  languages_in_order = {} 
+  languages.each do |style, language| 
+    language.each do |language, type|
+      type.each do |type, type_value|
+        if languages_in_order[language] = nil 
+          languages_in_order[language] = {}
+        end 
+        languages_in_order[language] = {type => type_value}
+        languages_in_order[language][:style] = array.new 
+        (languages_in_order[language][:style]) << style 
+        if (languages_in_order[language][:style]).include?(style) === false 
+          (languages_in_order[language][:style]).push(style)
+        end 
+      end 
+    end 
+  end 
+  languages.each do |style, language| 
+    language.each do |language, type| 
+      if (languages_in_order[language][:style]).include?(style) === false
+        (languages_in_order[language][:style]).push(style)
+      end 
+    end 
+  end 
+  return languages_in_order 
+end 
+
 end
